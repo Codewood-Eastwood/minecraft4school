b63f6aaaa0d364b24799c2b7ba819f7baf51db6f
ANGADSINGHT <angadpaltak@outlook.com>
2025-06-27 17:04:23 +0100

    Added Miside and structure change

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def image_help():
 @app.route('/minecraft-py')
 def download_minecraft_script():
     try:
-        with open("minecraft/script.py", "r") as f:
+        with open("games/minecraft/script.py", "r") as f:
             return f.read()
     except:  # noqa: E722
         return 'print("There was an unexpected error while reading the file to send :(")'  # noqa: E501
@@ -49,13 +49,13 @@ def download_minecraft_script():
 
 @app.route('/minecraft')  # Minecraft
 def download_minecraft():
-    return script_response("minecraft/run_minecraft_exe.ps")
+    return script_response("games/minecraft/run_minecraft_exe.ps")
 
 
 @app.route('/minecraft-exe')
 def download_script_exe():
     return send_file(
-        'minecraft/script.mp3',
+        'games/minecraft/script.mp3',
         as_attachment=True,
         download_name='script.mp3',
         mimetype='application/octet-stream'
@@ -65,7 +65,7 @@ def download_script_exe():
 @app.route('/download-polymc')
 def download_polymc():
     return send_file(
-        'minecraft/PolyMC.zip',
+        'games/minecraft/PolyMC.zip',
         as_attachment=True,
         download_name='PolyMC.mp3',
         mimetype='application/mp3'
@@ -75,7 +75,7 @@ def download_polymc():
 @app.route('/download-jdk17')
 def download_openjdk8():
     return send_file(
-        'minecraft/OpenJDK17.zip',
+        'games/minecraft/OpenJDK17.zip',
         as_attachment=True,
         download_name='OpenJDK17.mp3',
         mimetype='application/mp3'
@@ -84,28 +84,42 @@ def download_openjdk8():
 
 @app.route('/deltarune')
 def deltarune():
-    return script_response("deltarune/run_deltarune_exe.ps")
+    return script_response("games/deltarune/run_deltarune_exe.ps")
 
 
 @app.route('/deltarune-zip')
 def deltarune_zip():
     return send_file(
-        'deltarune/deltarunedemo.zip',
+        'games/deltarune/deltarunedemo.zip',
         as_attachment=True,
         download_name='deltarunedemo.zip',
         mimetype='application/zip'
     )
 
 
+@app.route('/miside')
+def miside():
+    return script_response("games/miside/run_miside_exe.ps")
+
+@app.route('/miside-zip')
+def miside_zip():
+    return send_file(
+        'games/miside/miside.zip',
+        as_attachment=True,
+        download_name='miside.zip',
+        mimetype='application/zip'
+    )
+
+
 @app.route('/madness-melee')
 def madness_melee():
-    return script_response("madness-melee/run_madness_melee_exe.ps")
+    return script_response("games/madness-melee/run_madness_melee_exe.ps")
 
 
 @app.route('/madness-melee-zip')
 def madness_melee_zip():
     return send_file(
-        'madness-melee/madness-melee.zip',
+        'games/madness-melee/madness-melee.zip',
         as_attachment=True,
         download_name='madness-melee.zip',
         mimetype='application/zip'
@@ -114,12 +128,12 @@ def madness_melee_zip():
 
 @app.route('/ddlc')
 def ddlc():
-    return script_response("ddlc/run_ddlc_exe.ps")
+    return script_response("games/ddlc/run_ddlc_exe.ps")
 
 @app.route('/ddlc-zip')
 def ddlc_zip():
     return send_file(
-        'ddlc/DDLC.zip',
+        'games/ddlc/DDLC.zip',
         as_attachment=True,
         download_name='DDLC.zip',
         mimetype='application/zip'
@@ -128,13 +142,13 @@ def ddlc_zip():
 
 @app.route('/steam')
 def steam():
-    return script_response("steam/run_steam_exe.ps")
+    return script_response("games/steam/run_steam_exe.ps")
 
 
 @app.route('/steam-zip')
 def steam_zip():
     return send_file(
-        'steam/Steam.exe',
+        'games/steam/Steam.exe',
         as_attachment=True,
         download_name='Steam.zip',
         mimetype='application/zip'
