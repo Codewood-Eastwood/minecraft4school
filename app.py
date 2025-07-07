f4f0d415c85c966706099763c97048222a01ad23
ANGADSINGHT <angadpaltak@outlook.com>
2025-07-07 21:39:43 +0100

    Reverted HTTPS, duckdns doesnt support

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ def delete_password():
     return password_manager(error)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=443, debug=True, ssl_context='adhoc')
+    app.run(host="0.0.0.0", port=443, debug=True)
